1ef04fd39bcf737393dcedbf1926a163e6b7b522
sorachatqq <pleumrinrin@gmail.com>
2026-01-08 19:47:51 +0700

    feat: commons filter&swagger

---
+++ src/common/filters/exception.filter.ts
@@ -0,0 +1,38 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const req = ctx.getRequest<Request>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : { message: 'Internal server error' };
+
+    res.status(status).json({
+      successful: false,
+      error_code: 'ERR_GENERIC',
+      data: null,
+      meta: {
+        path: req.url,
+        timestamp: new Date().toISOString(),
+        message,
+      },
+    });
+  }
+}
